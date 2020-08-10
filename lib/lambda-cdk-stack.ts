c2c37438116e83e2304f6f23c60043344f48dbcd
Cadell Christo <git@cadell.dev>
2020-08-10 17:22:20 +0800

    second commit, not quite working

--- lib/lambda-cdk-stack.ts
+++ lib/lambda-cdk-stack.ts
@@ -1,9 +1,24 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 
 export class LambdaCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const sendLinkLambda = new lambda.Function(this, 'sendLinkFunction', {
+      code: new lambda.AssetCode('src'),
+      handler: 'sendLink.handler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+    });
+
+    const api = new apigateway.RestApi(this, 'authApi', {
+      restApiName: 'Auth Service'
+    });
+
+    const authResource = api.root.addResource('auth');
+
+    const sendLinkIntegration = new apigateway.LambdaIntegration(sendLinkLambda);
+    authResource.addMethod('POST', sendLinkIntegration);
   }
 }
---
+++ src/sendLink.ts
@@ -0,0 +1,81 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+
+
+const allowedDomain = process.env.ALLOWED_DOMAIN || "cadell.dev";
+
+interface SendBody {
+	email: string;
+}
+
+export const sendLink: APIGatewayProxyHandler = async (event, _context) => {
+    // @ts-ignore
+	const data: SendBody = JSON.parse(event.body);
+	const { email } = data;
+	if (!email) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify({
+				validation: {
+					field: "email",
+					message: "Email is required",
+				},
+			}),
+		};
+	}
+
+	if (!email.endsWith("@" + allowedDomain)) {
+		return {
+			statusCode: 403,
+			body: JSON.stringify({
+				validation: {
+					field: "email",
+					message: "Email address is not allowed",
+				},
+			}),
+		};
+	}
+
+	// const tokenData: TokenData = {
+	// 	email,
+	// };
+
+	// const token = jwt.sign(tokenData, secret, { expiresIn: "1h" });
+    // 
+	// const params: SES.Types.SendEmailRequest = {
+	// 	Destination: {
+	// 		ToAddresses: [email],
+	// 	},
+	// 	Message: {
+	// 		Subject: {
+	// 			Data: "Magic link for internal auth service",
+	// 		},
+	// 		Body: {
+	// 			Html: {
+	// 				Data: `Token: ${token}`,
+	// 			},
+	// 			Text: {
+	// 				Data: `Token: ${token}`,
+	// 			},
+	// 		},
+	// 	},
+	// 	Source: "hello@cadell.dev",
+	// };
+
+	try {
+		// await ses.sendEmail(params).promise();
+		return {
+			statusCode: 200,
+			body: JSON.stringify({
+				message: "email sent succesfully",
+			}),
+		};
+	} catch (error) {
+		console.error(error, error.stack);
+		return {
+			statusCode: 503,
+			body: JSON.stringify({
+				message: "something went wrong :(",
+			}),
+		};
+	}
+};
